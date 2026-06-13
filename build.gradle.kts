7f8f2969d8e0c76014540817edaec7379b718abc
ahsanurrahman-sayem <ahsanurrahman10000@icloud.com>
2026-06-13 06:03:55 +0600

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,3 @@ plugins {
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22" apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
